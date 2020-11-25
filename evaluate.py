85a0c4f9c624f0bd9210fb9166a3dd700166d643
HarshSulakhe <sulakheharsh@gmail.com>
2020-11-25 15:36:41 +0530

    Add images, modify evaluate

--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,7 @@ import torchvision
 from torchvision import transforms
 import torchvision.datasets as dset
 from torchvision import transforms
+from torchvision.utils import save_image
 from mydataset import OmniglotTrain, OmniglotTest
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
@@ -22,12 +23,11 @@ if __name__ == '__main__':
     net = Siamese().cuda()
     net.eval()
 
-    net.load_state_dict(torch.load('models/model-inter-151.pt'))
+    net.load_state_dict(torch.load('models/FINAL.pt'))
 
     testSet = OmniglotTest('omniglot/python/images_evaluation', transform=transforms.ToTensor(), times = 400, way = 20)
     testLoader = DataLoader(testSet, batch_size=20, shuffle=False)
 
-    queue = deque(maxlen = 20)
 
     for i in range(20):
         right, error = 0, 0
@@ -38,15 +38,10 @@ if __name__ == '__main__':
             output = net.forward(test1, test2).data.cpu().numpy()
             pred = np.argmax(output)
             if pred == 0:
+                if right < 5:
+                    save_image(test1[0].squeeze(0),str(right)+'.jpg')
                 right += 1
             else: error += 1
         print('*'*70)
         print('\tTest set\tcorrect:\t%d\terror:\t%d\tprecision:\t%f'%( right, error, right*1.0/(right+error)))
         print('*'*70)
-        queue.append(right*1.0/(right+error))
-
-    final = 0
-    for acc in queue:
-        final += acc
-
-    print(final/20)
